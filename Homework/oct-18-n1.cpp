727a65927da32a172c246c2de56d488130eda9c2
TheIIIrd <idontuseemailforthat@idontneed.that>
2023-10-19 17:39:58 +0300

    Updated oct-18-n1.cpp

--- Homework/oct-18-n1.cpp
+++ Homework/oct-18-n1.cpp
@@ -13,10 +13,65 @@ using namespace std;
 
 // Получить случайное число в диапозоне от min до max
 int GetRandomNumber(int min, int max) {
-
     int num = min + rand() % (max - min + 1);
     return num;
 }
+
+vector<long int> VectGen() {
+
+    string ans;
+    cout << "Создать массив при помощи рандома ? [Y/n] : ";
+
+    cin.ignore();
+    getline(cin, ans);
+
+    int len;
+    vector<long int> vect_num;
+
+    if ((ans == "") || (ans[0] == 'y') || (ans[0] == 'Y') || (ans == "Yes") || (ans == "yes")) {
+
+        srand(time(NULL));
+        len = GetRandomNumber(20, 30);
+
+        cout << "Массив длиной " << len << " заполнен : " << endl;
+
+        for (int i = 0; i < len; ++i) {
+
+            vect_num.push_back(GetRandomNumber(-100, 100));
+            cout << vect_num[i] << ' ';
+
+        }
+
+        cout << endl;
+    }
+
+    else if ((ans[0] == 'n') || (ans[0] == 'N') || (ans == "no") || (ans == "No")) {
+
+        cout << "Введите длину массива : ";
+        cin >> len;
+
+        long double num;
+
+        for (int i = 0; i < len; ++i) {
+
+            cout << i << "-й элемент будет равен : ";
+            cin >> num;
+
+            vect_num.push_back(num);
+
+        }
+    }
+    
+    else {
+
+        cout << "Ответ введен некорректно" << endl;
+        return vect_num;
+
+    }
+
+    return vect_num;
+}
+
 
 /*
 Дан массив. Все его элементы:
@@ -36,59 +91,14 @@ int GetRandomNumber(int min, int max) {
 */
 void num_1() {
 
-    string ans;
-    cout << "Создать массив при помощи рандома ? [Y/n] : ";
+    vector<long int> vect_num;
+    vect_num = VectGen();
 
-    cin.ignore();
-    getline(cin, ans);
-
-    int len;
-    vector<long double> vect_num;
-
-    if ((ans == "") || (ans[0] == 'y') || (ans[0] == 'Y') || (ans == "Yes") || (ans == "yes")) {
-
-        srand(time(NULL));
-        len = GetRandomNumber(10, 30);
-
-        cout << "Массив длиной " << len << " заполнен : " << endl;
-
-        for (int i = 0; i <= len; ++i) {
-
-            vect_num.push_back(GetRandomNumber(-100, 100));
-            cout << vect_num[i] << ' ';
-
-        }
-
-        cout << endl;
-    }
-
-    else if ((ans[0] == 'n') || (ans[0] == 'n') || (ans == "no") || (ans == "No")) {
-
-        cout << "Введите длину массива : ";
-        cin >> len;
-
-        long double num;
-
-        for (int i = 0; i<= len; ++i) {
-
-            cout << i << "-й элемент будет равен : ";
-            cin >> num;
-
-            vect_num.push_back(num);
-
-        }
-    }
-    
-    else {
-
-        cout << "Ответ введен некорректно" << endl;
-        return;
-
-    }
+    size_t len = vect_num.size();
 
     // Увеличить в 2 раза
     cout << "[+] Массив увеличен в 2 раза :" << endl;
-    for (int i = 0; i <= len; ++i) {
+    for (size_t i = 0; i <= len; ++i) {
 
         vect_num[i] = vect_num[i] * 2;
         cout << vect_num[i] << ' ';
@@ -101,7 +111,7 @@ void num_1() {
     cin >> a;
 
     cout << "[+] Массив уменьшен на " << a << " :" << endl;
-    for (int i = 0; i <= len; ++i) {
+    for (size_t i = 0; i <= len; ++i) {
 
         vect_num[i] = vect_num[i] - a;
         cout << vect_num[i] << ' ';
@@ -114,7 +124,7 @@ void num_1() {
         cout << endl << "[+] Массив поделен на " << vect_num[0] << " :" << endl;
         long double first_vect_num = vect_num[0];
 
-        for (int i = 0; i <= len; ++i) {
+        for (size_t i = 0; i <= len; ++i) {
 
         vect_num[i] = vect_num[i] / first_vect_num;
         cout << vect_num[i] << ' ';
@@ -134,7 +144,7 @@ void num_1() {
     long double vect_sum = 0, vect_sq = 0, vect_six = 0, vect_ksix = 0, vect_saver = 0;
 
     // Сумма всех элементов массива и квадратов всех элементов массива
-    for (int i = 0; i <= len; ++i) {
+    for (size_t i = 0; i <= len; ++i) {
         vect_sum += vect_num[i];
         vect_sq += (vect_num[i] * vect_num[i]);
 
@@ -145,7 +155,7 @@ void num_1() {
 
     // Сумма шести первых элементов массива
     if (len >= 6) {
-        for (int i = 0; i <= 6; ++i) {
+        for (size_t i = 0; i <= 6; ++i) {
             vect_six += vect_num[i];
         }
         cout << "[+] Сумма шести первых элементов массива : " << vect_six << endl;
@@ -158,7 +168,7 @@ void num_1() {
     cout << "Введите k1 и k2 (границы подсчета суммы элементов) : ";
     cin >> k1 >> k2;
 
-    if ((k1 > k2) && (k1 <= len) && (k2 <= len)) {
+    if ((k1 > k2) && (k1 <= len) && (k2 <= len) && (k1 >= 0) && (k2 >= 0)) {
         for (int i = k2; i <= k1; ++i) {
 
             vect_ksix += vect_num[i];
@@ -167,7 +177,7 @@ void num_1() {
         cout << "[+] Сумма элементов массива от " << k2 << " до " << k1 << " : " << vect_ksix << endl;
     }
 
-    else if ((k1 <= len) && (k2 <= len)) {
+    else if ((k1 <= len) && (k2 <= len) && (k1 >= 0) && (k2 >= 0)) {
         for (int i = k1; i <= k2; ++i) {
 
             vect_ksix += vect_num[i];
@@ -186,7 +196,7 @@ void num_1() {
     cout << "Введите s1 и s2 (границы подсчета суммы элементов) : ";
     cin >> s1 >> s2;
 
-    if ((s1 > s2) && (s1 <= len) && (s2 <= len)) {
+    if ((s1 > s2) && (s1 <= len) && (s2 <= len) && (s1 >= 0) && (s2 >= 0)) {
         for (int i = s2; i <= s1; ++i) {
 
             vect_saver += vect_num[i];
@@ -196,7 +206,7 @@ void num_1() {
         cout << vect_saver / (s1 - s2 + 1) << endl;
     }
 
-    else if ((s1 <= len) && (s2 <= len)) {
+    else if ((s1 <= len) && (s2 <= len) && (s1 >= 0) && (s2 >= 0)) {
         for (int i = s1; i <= s2; ++i) {
 
             vect_saver += vect_num[i];
@@ -212,7 +222,79 @@ void num_1() {
 }
 
 
+/*
+Дан одномерный массив из 20 элементов. 
+Переставить первые три и последние три элемента, сохранив порядок их следования.
+*/
 void num_2() {
+
+    vector<long int> vect_num;
+    vect_num = VectGen();
+
+    size_t len = vect_num.size();
+
+    long double vnum;
+    
+    for (size_t i = 0; i < 3; ++i) {
+
+        vnum = vect_num[i];
+        vect_num[i] = vect_num[len - 3 + i];
+        vect_num[len - 3 + i] = vnum;
+
+    }
+
+    cout << "Масиив с переставленными элементами равен : " << endl;
+
+    for (size_t i = 0; i < len; ++i) {
+        cout << vect_num[i] << " ";
+    }
+
+    cout << endl;
+
+    return;
+}
+
+
+/*
+Удалить из массива:
+
+    - первый отрицательный элемент (если отрицательные элементы в массиве есть);
+    - последний четный элемент (если четные элементы в массиве есть);
+*/
+void num_3() {
+
+    vector<long int> vect_num;
+    vect_num = VectGen();
+
+    size_t len = vect_num.size();
+    long int neg_num = -1, even_num = -1;
+
+    for (size_t i = 0; i < len; ++i) {
+
+        if (vect_num[i] % 2 == 0) even_num = i;
+
+        else if ((vect_num[i] < 0) && (neg_num == -1)) neg_num = i;
+    }
+
+    if (even_num != -1) {
+        vect_num.erase(vect_num.begin() + even_num);
+
+        if (neg_num > even_num) neg_num -= 1;
+    }
+
+    if (neg_num != -1) {
+        vect_num.erase(vect_num.begin() + neg_num);
+    }
+
+    cout << "После обработки масиив равен : " << endl;
+
+    len = vect_num.size();
+    for (size_t i = 0; i < len; ++i) {
+        cout << vect_num[i] << " ";
+    }
+
+    cout << endl;
+
     return;
 }
 
@@ -230,6 +312,8 @@ int main () {
         case 1: num_1(); return 0;
 
         case 2: num_2(); return 0;
+
+        case 3: num_3(); return 0;
 
         default: 
             cout << "Задачи, под номером " << task_num << " не существует" << endl;
