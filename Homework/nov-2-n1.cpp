2386c2e6e21e03ad7f3ebfea2e256b08c42a6bbd
SuseIIIrd <ihatemailsystem@notformeallthis.idontlikethis>
2023-11-02 09:49:02 +0300

    added nov-2-n1.cpp

---
+++ Homework/nov-2-n1.cpp
@@ -0,0 +1,21 @@
+/*
+Лабораторная Работа "Калькулятор"
+*/
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+    string math_extract;
+
+    cout << "[пример] : ";
+    cin >> math_extract;
+
+    return 0;
+}
